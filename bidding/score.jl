9c532696531ee922625aeaa5775e31671f2765ef
Deniz Yuret <denizyuret@gmail.com>
2018-01-06 21:29:27 +0300

    parscore wip

--- bidding/score.jl
+++ bidding/score.jl
@@ -1,3 +1,28 @@
+# Trump suits
+const CLUBS=1
+const DIAMONDS=2
+const HEARTS=3
+const SPADES=4
+const NOTRUMP=5
+
+# Regular bids are represented by integers 1:35
+const TRUMPBIDS=35
+makebid(level,suit)=(5*(level-1)+suit)
+bidlevel(bid)=1+div(bid-1,5)
+bidsuit(bid)=mod1(bid,5)
+
+# Three extra bids
+const PASS=36
+const DOUBLE=37
+const REDOUBLE=38
+
+# Players
+WEST=1
+NORTH=2
+EAST=3
+SOUTH=4
+
+
 # Bridge scoring: based on 
 
 """
@@ -50,4 +75,46 @@ function imps(points)
     for i=1:length(a)
         if points < a[i]; return i-1; end
     end
+end
+
+
+# See http://bridgecomposer.com/Par.htm
+
+# Let us assume we have the following deal from library.gz:
+# JT852.93.KQ7.J82 AQ97.JT654.T6.A5 43.AK8.A542.7643 K6.Q72.J983.KQT9:88887777A9A977778888
+# Hands are given in W,N,E,S order.
+# Results are N/S tricks in hex grouped by NT,S,H,D,C.
+# Each group has 4 results with S,E,N,W leading.
+
+# The final contract reached by the model should specify trump, level,
+# double, vul info.  Vul and dealer can be picked randomly at the
+# beginning of bidding. The hands won comes from the DD and points() can
+# be used to calculate score.
+
+# The par score is the highest bid on which neither side can
+# improve. We can assume all down hands are doubled, and no final par
+# contract is redoubled.  Vul determined at start, dealer not
+# important, declarer should be the first person bidding the trump
+# suit but we can assume it can be freely chosen by the declaring
+# partnership.
+
+# Wrong: dealer may be important if both sides can make 1N, for
+# example, then the first one to bid gets the score.
+
+# The input is the 20 results and vulnerability, the output is the par N/S score.
+
+function parscore(hands, vul, dealer)
+    # par is the best n/s score so far and bid is the last bid so far.
+    # ns should return a higher par and a higher bid if possible.
+    function ns(par,bid)
+        for b = bid+1:BIDMAX
+        end
+    end
+    (par, bid, p, b) = (0, 0, -1, -1)
+    while (par,bid) != (p,b)
+        (p,b) = (par,bid)
+        (par,bid) = ns(par,bid)
+        (par,bid) = ew(par,bid)
+    end
+    return par
 end
